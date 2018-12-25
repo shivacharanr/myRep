8daf9ac451a208dc6c9d69d39db99208ff820cde
Shiva-Laptop <Shiva-Laptop@192.168.1.6>
2018-12-25 12:39:31 +0530

    first add

---
+++ maventest/src/main/java/package1/ClassA.java
@@ -0,0 +1,19 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	@Test
+	public void methodA1() {
+		System.out.println("methodA1 has been executed");
+	}
+	@Test
+	public void methodA2() {
+		System.out.println("methodA2 has been executed");
+	}
+	@Test
+	public void methodA3() {
+		System.out.println("methodA3 has been executed");
+	}
+
+}
---
+++ maventest/src/test/java/package2/ClassB.java
@@ -0,0 +1,19 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	@Test
+	public void methodB1() {
+		System.out.println("methodA1 has been executed");
+	}
+	@Test
+	public void methodB2() {
+		System.out.println("methodA2 has been executed");
+	}
+	@Test
+	public void methodB3() {
+		System.out.println("methodA3 has been executed");
+	}
+
+}
